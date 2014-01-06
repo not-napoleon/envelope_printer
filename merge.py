300120b2a7645063eef76d4f1f5cef111acff8d2
Mark Tozzi <mark.tozzi@gmail.com>
2014-01-05 19:12:47 -0500

    Process data from CSV file directly

--- merge.py
+++ merge.py
@@ -3,15 +3,16 @@ Python script for building LaTeX envlab formatted envelope list
 """
 import subprocess
 import os
+import csv
 
 # TODO: pull these from command line args
 TEMPLATE_FILE='envelopes_template.tex'
-DATA_FILE='addresses.dat'
+DATA_FILE='addresses.csv'
 RESULT_FILE = 'envelopes.tex'
 
 ADDRESS_LINE="\mlabel{%%\n%(from_address)s}{%%\n%(to_address)s}"
 
-def build_addresses(address_file):
+def build_addresses(to_addresses):
   """Build the formatted address list from the given file-like object
   """
   format_vars = {
@@ -20,13 +21,12 @@ def build_addresses(address_file):
   }
 
   addresses = []
-  with open(address_file) as infile:
-    for to_address in infile:
-      to_address = to_address.strip()
-      if to_address == '':
-        continue
-      format_vars['to_address'] = to_address
-      addresses.append(ADDRESS_LINE % format_vars)
+  for to_address in to_addresses:
+    to_address = to_address.strip()
+    if to_address == '':
+      continue
+    format_vars['to_address'] = to_address
+    addresses.append(ADDRESS_LINE % format_vars)
   return addresses
 
 def render_template(template_file, addresses):
@@ -40,11 +40,19 @@ def render_template(template_file, addresses):
 
 
 if __name__ == '__main__':
-  rendered = render_template(TEMPLATE_FILE, build_addresses(DATA_FILE))
+  to_addresses = []
+  with open(DATA_FILE, 'rb') as raw:
+    for line in csv.reader(raw):
+      line = [field.strip() for field in line if field != '']
+      tex_formatted = r"\\".join(line)
+      for char in ('&', '#'):
+        tex_formatted = tex_formatted.replace(char, r'\%s' % char)
+      to_addresses.append(tex_formatted)
+
+  rendered = render_template(TEMPLATE_FILE, build_addresses(to_addresses))
   with open(RESULT_FILE, 'w') as outfile:
     outfile.write(rendered)
 
   subprocess.call(['/opt/local/bin/pdflatex', RESULT_FILE],
       env={'TEXINPUTS': './envlab:'})
   os.unlink(RESULT_FILE)
-
